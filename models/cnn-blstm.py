1cdd1ce7b96f90f8b2bb8fe607f9ef42a2faa526
Ayushman Dash <dash.ayushman.99@gmail.com>
2019-02-06 14:23:08 +0530

    f1 score

--- models/cnn-blstm.py
+++ models/cnn-blstm.py
@@ -1,7 +1,7 @@
 import csv
 
 import numpy as np
-
+from keras import backend as K
 from tqdm import tqdm
 from bpemb import BPEmb
 from sklearn import preprocessing
@@ -14,17 +14,49 @@ from keras.layers import Conv1D, GlobalMaxPooling1D
 
 batch_size = 32
 embedding_dims = 50
-filters = 250
+filters = 300
 kernel_size = 3
 
-hidden_dims = 250
-epochs = 50
+hidden_dims = 500
+epochs = 1000
 bpemb_dims = 50
+
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
 
 bpemb_en = BPEmb(lang="en", dim=bpemb_dims)
 le = preprocessing.LabelEncoder()
 
-with open("../datasets/Chatbot/train.csv", "r") as f:
+with open("../datasets/WebApplication/train.csv", "r") as f:
     reader = csv.reader(f, delimiter="\t")
     max_len = 0
     y = []
@@ -42,7 +74,7 @@ y = to_categorical(encoded_labels, num_classes=len(le.classes_))
 
 x = None
 
-with open("../datasets/Chatbot/train.csv", "r") as f:
+with open("../datasets/WebApplication/train.csv", "r") as f:
     reader = csv.reader(f, delimiter="\t")
     for row in tqdm(reader):
         embeddings = bpemb_en.embed(row[0])
@@ -57,7 +89,7 @@ with open("../datasets/Chatbot/train.csv", "r") as f:
 
 x_test = None
 y_test = []
-with open("../datasets/Chatbot/test.csv", "r") as f:
+with open("../datasets/WebApplication/test.csv", "r") as f:
     reader = csv.reader(f, delimiter="\t")
     for row in tqdm(reader):
         embeddings = bpemb_en.embed(row[0])
@@ -75,7 +107,7 @@ print(x_test.shape, y_test.shape)
 print('Build model...')
 model = Sequential()
 
-model.add(Dropout(0.2))
+model.add(Dropout(0.5))
 
 # we add a Convolution1D, which will learn filters
 # word group filters of size filter_length:
@@ -99,7 +131,7 @@ model.add(Activation('softmax'))
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
-              metrics=['accuracy'])
+              metrics=[f1, "accuracy"])
 model.fit(x, y,
           batch_size=batch_size,
           epochs=epochs, validation_data=(x_test, y_test))
